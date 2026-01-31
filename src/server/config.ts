6320cf3945a94076fcf3e71f66e1934c6d66f9e5
lukasz <lukasz.pluszczewski@gmail.com>
2026-01-31 19:37:52 +0100

    Improve secret detection with allowlist and dev mode

--- src/server/config.ts
+++ src/server/config.ts
@@ -7,15 +7,25 @@ const booleanSchema = z
   .union([z.string(), z.number()])
   .transform((v: any) => v === 'true' || Boolean(parseInt(v)));
 
+// Config validation
 const configSchema = z.object({
   NODE_ENV: z.enum(['development', 'production']),
   port: z.coerce.number().min(1).max(65535),
 });
 
+// Config mapping
 const config = {
   NODE_ENV: process.env.NODE_ENV,
   port: process.env.PORT,
 } as const;
+
+// List of config keys containing secrets
+const SECRETS = [
+  'OPENAI_API_KEY',
+];
+
+// List of config keys that definitely not contain secrets
+const IGNORELIST = ['OPEN_AI_URL'];
 
 const parsedConfig = configSchema.safeParse(config);
 
@@ -27,8 +37,13 @@ if (!parsedConfig.success) {
 
 console.log(
   'Config loaded successfully',
-  map(parsedConfig.data, (value, key) =>
-    detectSecret(key, value) ? formatSecret(value) : value)
+  config.NODE_ENV === 'development' ? parsedConfig.data : map(parsedConfig.data, (value, key) => {
+    if (SECRETS.includes(key)) return formatSecret(value);
+    if (detectSecret(key, value, { ignorelist: IGNORELIST })) {
+      throw new Error(`"${key}" seems to be a secret. Add it to ignorelist or to "SECRETS" array in config.ts`)
+    }
+    return value;
+  })
 );
 
 
